fe4026bb3fc2e70f05dc3bb05f0e663d6fc7d128
yaknafo <knafoyair@gmail.com>
2015-05-17 12:35:52 +0300

    z

--- BasisForAppraisal_finalProject/BasisForAppraisal_finalProject/DBML/FormPartial.cs
+++ BasisForAppraisal_finalProject/BasisForAppraisal_finalProject/DBML/FormPartial.cs
@@ -11,6 +11,8 @@ namespace BasisForAppraisal_finalProject.DBML
 
         private List<tbl_Section> sections = new List<tbl_Section>();
 
+        private List<tbl_Company> companies;
+
 
 
         public List<tbl_Section> Sections
@@ -27,6 +29,19 @@ namespace BasisForAppraisal_finalProject.DBML
 
         }
 
+        public List<tbl_Company> Companies
+        {
+            get
+            {
+                return companies ?? GetAllCompanies();
+            }
+            set
+            {
+                companies = value;
+            }
+
+        }
+
 
 
 
@@ -34,6 +49,14 @@ namespace BasisForAppraisal_finalProject.DBML
         {
             var manager = new DataManager();
             return manager.Sections.Where(x => x.FormId == this.formId).ToList();
+        }
+
+
+        public List<tbl_Company> GetAllCompanies()
+        {
+            var DMC = new DataMangerCompany();
+            companies = DMC.ConnectorFormFill.Where(x => x.formId == formId).Select(c => c.tbl_Company).ToList();
+            return companies;
         }
 
       
---
+++ BasisForAppraisal_finalProject/BasisForAppraisal_finalProject/ViewModel/ViewModelFromsDetalis.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BasisForAppraisal_finalProject.DBML;
+using BasisForAppraisal_finalProject.Models;
+
+namespace BasisForAppraisal_finalProject.ViewModel
+{
+    public class ViewModelFromsDetalis
+    {
+        public List<tblForm> forms { set; get; }
+
+        public ViewModelFromsDetalis(List<tblForm> forms)
+        {
+            this.forms = forms;
+        }
+
+
+      
+    }
+}
