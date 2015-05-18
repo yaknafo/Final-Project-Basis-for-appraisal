20c570b83495ce942cbb001afa0e65269571c61e
orfrenkel <orf@zofim.org.il>
2015-05-18 23:03:27 +0300

    changePopUP and email

--- BasisForAppraisal_finalProject/BasisForAppraisal_finalProject/Controllers/SendMailerController.cs
+++ BasisForAppraisal_finalProject/BasisForAppraisal_finalProject/Controllers/SendMailerController.cs
@@ -6,6 +6,8 @@ using System.Web;
 using System.Web.Mvc;
 using BasisForAppraisal_finalProject.DBML;
 using BasisForAppraisal_finalProject.Models;
+using BasisForAppraisal_finalProject.Controllers;
+
 namespace SendMail.Controllers
 {
     public class SendMailerController : Controller
@@ -17,31 +19,50 @@ namespace SendMail.Controllers
         public ActionResult Index(int id,string unit,string cl)
         {
             DataMangerCompany dm = new DataMangerCompany();
+            List<tbl_Employee> NotSendMail = new List<tbl_Employee>();
+            string body = "שלום רב\n" +
+          "מייל זה נשלח לך על ידי בסיס להערכה\n" +
+        "  למייל זה מצורף לינק  לאתר החברה\n" +
+          "בכניסה לאתר תצטרך להכניס שם משתמש וסיסמא אשר מצורפים למייל זה \n" +
+          "לאחר כניסה לאתר תוכל לצפות בכל הטפסים אשר עלייך למלא\n" +
+          "קישור לאתר:\n";
+            String name = " :שם משתמש\n";
+            String password = " סיסמא:\n\n\n";
+            string endbody = "תודה צוות בסיס להערכה";
 
             foreach (var emp in dm.getEmpForEmail(id,unit,cl))
             {
                 BasisForAppraisal_finalProject.Models.MailModel _objModelMail = new BasisForAppraisal_finalProject.Models.MailModel();
                 if (ModelState.IsValid)
                 {
-                    _objModelMail.From = "dontreplaybasisforappraisal@gmail.com";
-                    _objModelMail.Body = "בדיקת מייל";
-                    _objModelMail.Subject = "בסיס להערכה- שאלון למילוי";
-                    _objModelMail.To = emp.Email;
-                    MailMessage mail = new MailMessage();
-                    mail.To.Add(_objModelMail.To);
-                    mail.From = new MailAddress(_objModelMail.From);
-                    mail.Subject = _objModelMail.Subject;
-                    string Body = _objModelMail.Body;
-                    mail.Body = Body;
-                    mail.IsBodyHtml = true;
-                    SmtpClient smtp = new SmtpClient();
-                    smtp.Host = "smtp.gmail.com";
-                    smtp.Port = 587;
-                    smtp.UseDefaultCredentials = false;
-                    smtp.Credentials = new System.Net.NetworkCredential
-                    ("dontreplaybasisforappraisal@gmail.com", "basis123456");// Enter seders User name and password
-                    smtp.EnableSsl = true;
-                    smtp.Send(mail);
+                    try
+                    {
+                        _objModelMail.From = "dontreplaybasisforappraisal@gmail.com";
+                        _objModelMail.Body = body + name + emp.employeeId + password + emp.employeeId + endbody;
+
+                        _objModelMail.Subject = "בסיס להערכה- שאלון למילוי";
+                        _objModelMail.To = emp.Email;
+                        MailMessage mail = new MailMessage();
+                        mail.To.Add(_objModelMail.To);
+                        mail.From = new MailAddress(_objModelMail.From);
+                        mail.Subject = _objModelMail.Subject;
+                        string Body = _objModelMail.Body;
+                        mail.Body = Body;
+                        mail.IsBodyHtml = true;
+                        SmtpClient smtp = new SmtpClient();
+                        smtp.Host = "smtp.gmail.com";
+                        smtp.Port = 587;
+                        smtp.UseDefaultCredentials = false;
+                        smtp.Credentials = new System.Net.NetworkCredential
+                        ("dontreplaybasisforappraisal@gmail.com", "basis123456");// Enter seders User name and password
+                        smtp.EnableSsl = true;
+                        smtp.Send(mail);
+                    }
+                    catch(Exception ex)
+                    {
+                        NotSendMail.Add(emp);
+                    }
+                
                 }
             }
 
