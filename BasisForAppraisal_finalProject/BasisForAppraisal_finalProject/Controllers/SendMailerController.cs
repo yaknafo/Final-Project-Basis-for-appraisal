f9cd59d79929a9a2fa9c158d07edd3224dc1c09b
orfrenkel <orf@zofim.org.il>
2015-05-26 22:11:40 +0300

    change url mail

--- BasisForAppraisal_finalProject/BasisForAppraisal_finalProject/Controllers/SendMailerController.cs
+++ BasisForAppraisal_finalProject/BasisForAppraisal_finalProject/Controllers/SendMailerController.cs
@@ -34,7 +34,7 @@ namespace SendMail.Controllers
                           "<br /בכניסה לאתר תצטרך להכניס שם משתמש וסיסמא אשר מצורפים למייל זה <br />" +
                           "<br />לאחר כניסה לאתר תוכל לצפות בכל הטפסים אשר עלייך למלא<br />" +
                           "<br />:קישור לאתר<br />" +
-                          "<br />http://localhost:21981/<br />";
+                          "<br />http://localhost:21981/Account/Login<br />";
             String name = "<br /> שם משתמש:";
             String password = "<br /> סיסמא:";
            
